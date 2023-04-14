3494d39f868b7626bf2a631e04fb2c9bd8eb33b7
Jakob Norlin <jakob.stahl91@gmail.com>
2023-04-14 13:33:36 +0200

    Remove vscode setup
    It's synced through github now

--- update.sh
+++ update.sh
@@ -3,14 +3,6 @@ NC='\033[0m' # No color
 
 echo "${PURPLE}Updating configs...${NC}"
 
-### VS Code config ###
-echo "${PURPLE}Updating VS Code configs...${NC}"
-mkdir -p ~/Library/Application\ Support/Code/User
-rm -f ~/Library/Application\ Support/Code/User/keybindings.json
-rm -f ~/Library/Application\ Support/Code/User/settings.json
-ln ./vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json 
-ln ./vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
-
 ### Z shell config file ###
 echo "${PURPLE}Updating Z shell config...${NC}"
 rm -f ~/.zshrc
